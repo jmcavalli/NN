73591a5b3f479065bb2614e84900193afdd84d41
jmcavalli <jmcavalli@ucdavis.edu>
2017-03-03 12:27:20 -0800

    "Got it reading in the files"

--- NN Project/src/nn/project/NNProject.java
+++ NN Project/src/nn/project/NNProject.java
@@ -5,6 +5,8 @@
  */
 package nn.project;
 
+import java.io.*;
+
 /**
  *
  * @author bible_000
@@ -16,7 +18,36 @@ public class NNProject {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World!");
+        String filename = System.getProperty("user.dir") + "\\Female\\5_1_1.txt";
+        String line;
+        
+        try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader = 
+                new FileReader(filename);
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader = 
+                new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }   
+
+            // Always close files.
+            bufferedReader.close();         
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" + 
+                filename + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + filename + "'");                  
+        }
     }
+        
     
 }
